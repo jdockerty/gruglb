f6a4035a143b6649f6f83830f70f2eb9a6d2cea6
Jack <56563911+jdockerty@users.noreply.github.com>
2023-10-11 12:01:44 +0100

    feat: handle multiple connections (#1)
    * initial work with threads
    
    * add clarifying comments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::{io, thread};
+use serde_json::json;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-fn main() -> io::Result<()> {
-    let bind_addrs = vec!["9091", "9092"];
+use rand::prelude::*;
 
-    let mut listeners = Vec::new();
+fn work(name: &str, stream: TcpStream) {
+    println!("{name} starting work");
+    let res = json!({"status": "OK"});
+    thread::sleep(std::time::Duration::from_secs(5));
+    serde_json::to_writer(stream, &res);
+    println!("{name} done!");
+}
 
-    for b in bind_addrs {
-        let addr = &format!("127.0.0.1:{}", b);
-        let listener = TcpListener::bind(addr).unwrap();
+//fn handle_client(mut stream: TcpStream, backend_addr: &str) {
+//    let mut backend_stream = TcpStream::connect(backend_addr).unwrap();
+//
+//    let mut buffer = [0; 512];
+//    loop {
+//        let nbytes = stream.read(&mut buffer).unwrap();
+//        if nbytes == 0 {
+//            break;
+//        }
+//        backend_stream.write(&buffer[..nbytes]).unwrap();
+//    }
+//}
+
+fn main() {
+    let ports = vec!["9091", "9092"];
+
+    for port in ports {
+        let addr = format!("127.0.0.1:{}", port);
+        let listener = TcpListener::bind(&addr).unwrap();
         println!("Listening on {}", addr);
-        listeners.push(listener);
-    }
 
-    for l in listeners {
+        // Listen to incoming traffic on separate threads
         thread::spawn(move || {
-            for stream in l.incoming() {
-                let stream = stream.unwrap();
-                println!("{:?}", stream.local_addr());
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        // Pass the TCP streams over to separate threads to avoid
+                        // blocking.
+                        thread::spawn(move || {
+                            let mut rng = thread_rng();
+                            let n: u32 = rng.gen();
+                            let name = &format!("thread-{}", n);
+                            work(name, stream);
+                        });
+                    }
+                    Err(e) => {
+                        eprintln!("Unable to connect: {}", e);
+                    }
+                }
             }
         });
     }
 
+    // Sleep main thread so spawned threads can run
     thread::park();
-    Ok(())
 }
