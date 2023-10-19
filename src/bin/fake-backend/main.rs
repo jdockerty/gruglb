8487f946949288968d34b4d0245bf5a7e50f3a8a
Jack Dockerty <jdockerty19@gmail.com>
2023-10-19 08:38:43 +0100

    pass fake-backend server id

--- src/bin/fake-backend/main.rs
+++ src/bin/fake-backend/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use std::{io::Write, net::TcpListener};
-use uuid;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -11,11 +10,13 @@ struct Cli {
 
     #[arg(long, default_value = "tcp")]
     protocol: String,
+
+    #[arg(long)]
+    id: String,
 }
 
 fn main() {
     let args = Cli::parse();
-    let uuid = uuid::Uuid::new_v4();
     let protocol = match args.protocol.to_lowercase().as_str() {
         "http" => "http".to_string(),
         _ => "tcp".to_string(),
@@ -26,11 +27,11 @@ fn main() {
     } else {
         let addr = TcpListener::bind(format!("127.0.0.1:{}", args.port)).unwrap();
 
-        println!("[{}] Listening on {}", uuid, addr.local_addr().unwrap());
+        println!("[{}] Listening on {}", args.id, addr.local_addr().unwrap());
 
         while let Ok((mut stream, addr)) = addr.accept() {
             println!("Incoming from {}", addr);
-            let buf = String::from(format!("Hello from backend {}", uuid));
+            let buf = String::from(format!("Hello from {}", args.id));
             stream.write_all(buf.as_bytes()).unwrap();
         }
     }
