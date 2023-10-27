0c8ba09560ba6308bb480969136113d8e6c497a4
Jack Dockerty <jdockerty@influxdata.com>
2023-10-27 18:08:15 +0100

    add http_connection func

--- src/proxy.rs
+++ src/proxy.rs
@@ -175,7 +175,75 @@ where
             }
         };
     } else {
-        info!("No backend configured");
+        info!("[TCP] No backend configured");
+    };
+
+    Ok(())
+}
+
+pub fn http_connection<S>(
+    targets: Arc<RwLock<HashMap<String, Vec<Backend>>>>,
+    target_name: String,
+    routing_idx: Arc<Mutex<usize>>,
+    method: String,
+    request_path: String,
+    mut stream: S,
+) -> Result<()>
+where
+    S: Read + Write,
+{
+    if let Some(backends) = targets.read().unwrap().get(&target_name) {
+        let backends = backends.to_vec();
+        debug!("Backends configured {:?}", &backends);
+        let backend_count = backends.len();
+
+        if backend_count == 0 {
+            info!("[HTTP] No routable backends for {target_name}, nothing to do");
+            return Ok(());
+        }
+
+        let mut idx = match routing_idx.lock() {
+            Ok(idx) => idx,
+            Err(poisoned) => poisoned.into_inner(),
+        };
+
+        debug!("[HTTP] {backend_count} backends configured for {target_name}, current index {idx}");
+
+        // Reset index when out of bounds to route back to the first server.
+        if *idx >= backend_count {
+            *idx = 0;
+        }
+
+        let http_backend = format!(
+            "http://{}:{}{}",
+            backends[*idx].host, backends[*idx].port, request_path
+        );
+
+        // Increment a shared index after we've constructed our current connection
+        // address.
+        *idx += 1;
+
+        info!("[HTTP] Attempting to connect to {}", &http_backend);
+        let client = reqwest::blocking::Client::new();
+
+        match method.as_str() {
+            "GET" => {
+                let backend_response = client.get(http_backend).send()?;
+
+                let version = backend_response.version();
+                debug!("RESP: {:?}", backend_response.text()?);
+                // TODO: Send response back to caller via stream.
+            }
+            "POST" => {
+                let response = client.post(http_backend).send()?;
+                stream.write_all(&response.bytes()?)?;
+            }
+            _ => {
+                error!("Unsupported: {method}")
+            }
+        }
+    } else {
+        info!("[HTTP] No backend configured");
     };
 
     Ok(())
@@ -200,7 +268,23 @@ fn get_tcp_listeners(
     Ok(tcp_bindings)
 }
 
-fn get_http_backends() {}
+fn get_http_listeners(
+    bind_address: String,
+    targets: HashMap<String, Target>,
+) -> Result<Vec<(String, TcpListener)>> {
+    let mut http_bindings = vec![];
+
+    for (name, target) in targets {
+        if target.protocol_type() == Protocol::Http {
+            let addr = format!("{}:{}", bind_address.clone(), target.listener.unwrap());
+            let listener = TcpListener::bind(&addr)?;
+            info!("Binding to {} for {}", &addr, &name);
+            http_bindings.push((name, listener));
+        }
+    }
+
+    Ok(http_bindings)
+}
 
 pub fn accept_http(
     bind_address: String,
@@ -208,34 +292,51 @@ pub fn accept_http(
     targets: HashMap<String, Target>,
 ) -> Result<()> {
     let idx: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
-    let http_bind = TcpListener::bind(format!("{bind_address}:8080"))?;
+    let bound_listeners = get_http_listeners(bind_address, targets)?;
 
-    for stream in http_bind.incoming() {
-        let current_healthy_targets = Arc::clone(&current_healthy_targets);
-        thread::spawn(move || match stream {
-            Ok(mut stream) => {
-                info!("Incoming HTTP request");
-                use std::io::prelude::*;
-                let buf = BufReader::new(&mut stream);
+    for (name, listener) in bound_listeners {
+        for stream in listener.incoming() {
+            let name = name.clone();
+            let idx = Arc::clone(&idx);
+            let current_healthy_targets = Arc::clone(&current_healthy_targets);
+            thread::spawn(move || match stream {
+                Ok(mut stream) => {
+                    info!("Incoming HTTP request");
+                    use std::io::prelude::*;
+                    let buf = BufReader::new(&mut stream);
 
-                let http_request: Vec<_> = buf
-                    .lines()
-                    .map(|result| result.unwrap())
-                    .take_while(|line| !line.is_empty())
-                    .collect();
+                    let http_request: Vec<_> = buf
+                        .lines()
+                        .map(|result| result.unwrap())
+                        .take_while(|line| !line.is_empty())
+                        .collect();
 
-                let info = &http_request[0];
-                let http_info = info.split_whitespace().collect::<Vec<&str>>();
+                    let info = http_request[0].clone();
+                    let http_info = info
+                        .split_whitespace()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<_>>();
 
-                let method = http_info[0];
-                let path = http_info[1];
+                    let method = http_info[0].clone();
+                    let path = http_info[1].clone();
 
-                info!("{method} request at {path}");
-            }
-            Err(e) => {
-                error!("Unable to connect: {}", e);
-            }
-        });
+                    debug!("{method} request at {path}");
+                    thread::spawn(move || {
+                        http_connection(
+                            current_healthy_targets,
+                            name,
+                            idx,
+                            method.to_string(),
+                            path.to_string(),
+                            stream,
+                        )
+                    });
+                }
+                Err(e) => {
+                    error!("Unable to connect: {}", e);
+                }
+            });
+        }
     }
 
     Ok(())
