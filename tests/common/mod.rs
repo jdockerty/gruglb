24ca040fee04b179369af03f8ad5daf26dfb05d4
Jack Dockerty <jdockerty@influxdata.com>
2023-11-01 17:30:19 +0000

    feat: add integration test for healthy targets

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,16 +3,9 @@ use gruglb::lb::{RecvTargets, SendTargets};
 use std::fs::File;
 use std::sync::mpsc::sync_channel;
 
-pub fn single_tcp_target_config() -> config::Config {
+pub fn test_targets_config() -> config::Config {
     let fake_conf =
-        File::open("tests/fixtures/single-tcp-target.yaml").expect("unable to open example config");
-
-    config::new(fake_conf).unwrap()
-}
-
-pub fn single_http_target_config() -> config::Config {
-    let fake_conf = File::open("tests/fixtures/single-http-target.yaml")
-        .expect("unable to open example config");
+        File::open("tests/fixtures/example-config.yaml").expect("unable to open example config");
 
     config::new(fake_conf).unwrap()
 }
