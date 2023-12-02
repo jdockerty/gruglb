16184006821aa8da5a7de4c8c5d20b4fff94836d
Jack Dockerty <jdockerty@influxdata.com>
2023-12-02 16:59:23 +0000

    feat: optional graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use tracing::{error, info};
 #[tokio::main]
 async fn main() -> Result<()> {
     let lb = init()?;
+    let graceful = lb.conf.graceful_shutdown();
     let channel_size = lb.conf.target_names().map_or(2, |targets| targets.len());
     let (send, recv): (SendTargets, RecvTargets) = channel(channel_size);
 
@@ -15,18 +16,23 @@ async fn main() -> Result<()> {
 
     lb.run(send, recv, token.child_token()).await?;
 
-    match tokio::signal::ctrl_c().await {
-        Ok(_) => {
-            token.cancel();
-            info!("RECEIVED CANCEL OPERATION, COMMENCING 30 GRACE PERIOD BEFORE SHUTDOWN");
-            // Display the shut down message 6 times, every 5 seconds before terminating.
-            for i in 1..=6 {
-                info!("[{i}/6] CANCEL OPERATION RECEIVED: TERMINTATING PROCESSES");
-                tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    if graceful {
+        match tokio::signal::ctrl_c().await {
+            Ok(_) => {
+                token.cancel();
+                info!("Cancel operation received, waiting 30s before shutdown.");
+                // Display the shut down message 6 times, every 5 seconds before terminating.
+                for i in 1..=6 {
+                    info!("[{i}/6] Cancel operation received, terminating processes.");
+                    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+                }
+                info!("30 second grace period has elapsed, shutting down.");
             }
-            info!("30 second grace period has elapsed, shutting down.");
+            Err(e) => error!("Error sending SIGINT: {e}"),
         }
-        Err(e) => error!("Error sending SIGINT: {e}"),
+    } else {
+        info!("Running with no grace period");
+        tokio::signal::ctrl_c().await.unwrap();
     }
 
     Ok(())
